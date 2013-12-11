e7add546f07b38754f686ddaaa887b49cb527f99
Anas Nakawa <anas.nakawa@gmail.com>
2013-12-11 17:50:32 +0400

    updated converted indentations

--- jDeclarative.js
+++ jDeclarative.js
@@ -11,44 +11,44 @@
 
   // injecting version via grunt
   // ---------------------------
-  var _version = '0.0.1';	
+  var _version = '0.0.1';  
 
-	// parse bindings
-	// --------------
-	function parseBindings( attr ) {
-		return new Function( 'return { ' + attr + ' };' )();
-	}
+  // parse bindings
+  // --------------
+  function parseBindings( attr ) {
+    return new Function( 'return { ' + attr + ' };' )();
+  }
 
-	// class definition
-	// ----------------
-	function Declarative( element, options ) {
-		this.element = element;
-		this.options = options;
+  // class definition
+  // ----------------
+  function Declarative( element, options ) {
+    this.element = element;
+    this.options = options;
 
-		// initialize
-		this.init();
-	}
+    // initialize
+    this.init();
+  }
 
-	// default settings
-	Declarative.defaults = {};
-	Declarative.version = _version;
+  // default settings
+  Declarative.defaults = {};
+  Declarative.version = _version;
 
-	Declarative.prototype.init = function() {
+  Declarative.prototype.init = function() {
 
-	};
+  };
 
-	$.declarative = {
-		defaults: Declarative.defaults
-	};
+  $.declarative = {
+    defaults: Declarative.defaults
+  };
 
-	$.fn.declarative = function( options ) {
+  $.fn.declarative = function( options ) {
 
-		// merge options
-		options = $.extend( $.declarative.defaults, options );
+    // merge options
+    options = $.extend( $.declarative.defaults, options );
 
-		return this.each( function() {
-			new Declarative( this, options );
-		});		
-	}
+    return this.each( function() {
+      new Declarative( this, options );
+    });   
+  }
 
 })( jQuery );

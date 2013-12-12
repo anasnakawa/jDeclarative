a73f0d54c9b8a542ea3d7b9e5493a176825d5b7a
Anas Nakawa <anas.nakawa@gmail.com>
2013-12-12 18:01:53 +0400

    removed un-needed util methods

--- jDeclarative.js
+++ jDeclarative.js
@@ -11,83 +11,61 @@
 
   // injecting version via grunt
   // ---------------------------
-  var _version = '0.1.0';  
-
-  // string tiny template
-  // --------------------
-  // parse a given template and repalce any variables wrapped with brackets '{' & '}' with the
-  // corresponding object found in the passed context param
-  // * **param:** {string} template    sting template to be parsed
-  // * **param:** {object} context     object containing variables to inject into the template
-  //
-  // e.g: parse( 'hello my name is {name}, I am a {title}', { 
-  //      name: 'Anas Nakawa'
-  //      title: function() {
-  //          return 'software developer'
-  //      }
-  // });     
-  // >> 'hello my name is Anas Nakawa, I am a software developer'
-  function parse( template, context ) {
-    return template.replace(/{([A-Za-z0-9_$\-]*)}/g, function(token, match){
-      if( !match in context ) {
-        return;
-      }
-      return ( typeof context[match] === 'function' ) ? context[match]() : context[match];
-    });
-  }
-
-  // parse bindings
-  // --------------
-  function parseBindings( attr ) {
-    var parsed;
-    try {
-      parsed = new Function( 'return { ' + attr + ' };' )();
-    } catch( e ) {
-      throw new Error( parse( 'unable to parse binding: {binding} \n message: {error}', {
-          binding: attr
-        , error: e
-      } ) );
-    }
-    return parsed;
-  }
+  var _version = '0.1.0'; 
 
   // class definition
   // ----------------
+  // @param {dom} element
+  // @param {object} options
   function Declarative( element, options ) {
     this.element = element;
     this.options = options;
-    this.version = _version;
-
-    // initialize
-    this.init();
   }
 
-  // store auto generated version
-  Declarative.version = _version;
-
-  // default settings
-  Declarative.defaults = {
-    attr: 'data-plugin'
-  };  
 
   Declarative.prototype.init = function() {
     var self = this
-    , parsedAttribute = parseBindings( this.element.getAttribute( this.options.attr ) );
+    , parsedAttribute = Declarative.parseBindings( this.element.getAttribute( this.options.attr ) );
 
     $.each( parsedAttribute, function( plugin, options ) {
-      // debugger;
       $( self.element )[ plugin ]( options );
     });
   };
 
+  // store auto generated version
+  Declarative.version = _version;
+
+  // parse bindings
+  // --------------
+  // @param {string} attr 
+  // @return {object} parsed binding
+  Declarative.parseBindings = function( attr ) {
+    var parsed;
+    try {
+      parsed = new Function( 'return { ' + attr + ' };' )();
+    } catch( e ) {
+      throw new Error( 'unable to parse binding: ' + attr + ' \n message: ' + e );
+    }
+    return parsed;
+  }
+
+  
+  // default settings
+  Declarative.defaults = {
+    attr: 'data-plugin'
+  };
+
+
+  // jQuery plugin
+  // -------------
   $.fn.declarative = function( options ) {
 
     // merge options
-    options = $.extend( Declarative.defaults, options );
+    options = $.extend( {}, Declarative.defaults, options );
 
     return this.each( function() {
       return $( this ).find( '[{attr}]'.replace( /{attr}/, options.attr ) ).each( function() {
-        new Declarative( this, options );
+        new Declarative( this, options ).init();
       });
     });   
   }
